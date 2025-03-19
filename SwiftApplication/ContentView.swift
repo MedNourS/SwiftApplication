37ebefcce1de1f076660916125568c699fbe3b63
MedNourS <152449958+MedNourS@users.noreply.github.com>
2025-03-19 15:57:13 +0000

    Calculator App made

--- SwiftApplication/ContentView.swift
+++ SwiftApplication/ContentView.swift
@@ -8,12 +8,68 @@
 import SwiftUI
 
 struct ContentView: View {
+
+    // Variables
+    @State private var num1: Int = 0
+    @State private var num2: Int = 0
+    @State private var result: Int = 0
+
+    // Functions
+    func sum(of a: Double, and b: Double) -> Double {
+        return a + b
+    }
+
+    func difference(of a: Double, and b: Double) -> Double {
+        return a - b
+    }
+
+    func product(of a: Double, and b: Double) -> Double{
+        return a * b
+    }
+
+    func quotient(of a: Double, and b: Double) -> Double {
+        return a / b
+    }
+
+    func updateResult(with function: (num1, num2) -> Double) {
+        result = function
+        print("The result has been updated to: \(result)")
+    }
+
+    func clearResult() {
+        result = 0
+    }
+
+    // View
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            TextField("Number 1", value: $num1, format: .number)
+            TextField("Number 2", value: $num2, format: .number)
+            
+            HStack {
+                Button(action: updateResult(sum)) {
+                    systemImage: "addition"
+                }
+
+                Button(action: updateResult(difference)) {
+                    systemImage: "substraction"
+                }
+
+                Button(action: updateResult(product)) {
+                    systemImage: "multiplication"
+                }
+
+                Button(action: updateResult(quotient)) {
+                    systemImage: "division"
+                }
+            }
+            .padding()
+
+            Button(action: clearResult()) {
+                systemImage: "circle"
+            }
+
+            Text("Result: \(result)")
         }
         .padding()
     }
